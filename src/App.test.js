6287d11b4059b1164d1d54f02b991ff4c4f608b7
Pari <pari.ramamoorthy@gmail.com>
2021-08-25 14:12:33 -0400

    react-context-hook-starterkit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { shallow } from "enzyme";
+
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+it("renders without crashing", () => {
+  shallow(<App />);
+});
+
+it("renders Account header", () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h1>React Context API</h1>;
+  expect(wrapper.contains(welcome)).toEqual(true);
 });
